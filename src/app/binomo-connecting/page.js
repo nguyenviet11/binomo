4071173fbc3657b32ab1c9bd67c45121a3fba841
vietnv <nguyenvanvietbn1998@gmail.com>
2025-06-30 13:11:48 +0700

    sss

--- src/app/binomo-connecting/page.js
+++ src/app/binomo-connecting/page.js
@@ -16,7 +16,7 @@ export default function page() {
     <div>
       <main className={styles.main}>
         <h3 className={styles.text}>
-        Binomo Official Site{" "}
+        Binomo Official Site{""}
           <span
             style={{ transform: "translate(0px)", display: "inline-block" }}
           >
